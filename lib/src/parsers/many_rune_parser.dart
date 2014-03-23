52daab7ac8cf7f6a00f2b86c3099e178f3aa8ba6
David Bordoley <bordoley@gmail.com>
2014-03-22 23:53:48 -0700

    fix many rune parser.

--- lib/src/parsers/many_rune_parser.dart
+++ lib/src/parsers/many_rune_parser.dart
@@ -8,8 +8,9 @@ class _ManyRuneParser extends ParserBase<IterableString> {
   ParseResult<IterableString> parseFrom(final IterableString str) {
     final CodePointIterator itr = str.iterator;
     while(itr.moveNext() && matcher.matches(itr.current));
-    final IterableString result = str.substring(0, itr.index + 1);
-    return new ParseResult.success(result, str.substring(itr.index + 1));
+    itr.movePrevious();
+    return new ParseResult.success(
+        str.substring(0, itr.index + 1), str.substring(itr.index + 1));
   }
 
   String toString() =>
